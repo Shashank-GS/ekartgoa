1bc7eb020d25191b5d4d6e97b242f1b062edfbf5
shashankgovindshetti9@gmail.com <shashankgovindshetti9@gmail.com>
2020-08-05 14:38:45 +0530

    cart adding success ekartgoa

--- ekart.js
+++ ekart.js
@@ -1,4 +1,6 @@
 // alert("Welcome to Ekart"); 
+
+ var totalamount=0;
 function myCart(elem) {
     var l = Number(elem.id)
     console.log(typeof(l))
@@ -15,15 +17,33 @@ function myCart(elem) {
       node1.appendChild(textnode1);
       document.getElementById("cartprice").appendChild(node1);
 
+      var str = document.getElementsByClassName('price')[i].textContent;
+      // var itemprice = parseInt(textnode1)
+      // console.log(String(textnode1))
+      var  str1  = str.substring(1); 
+      var str2= str1.replace(',', '');
+      console.log(str2) ;  
+      // var  substr  = str.substring(1); 
+      console.log(typeof(str2)) 
+      var itemprice = Number(str2)
 
-      //   var item1 = document.getElementsByClassName('name')[i].textContent;
-      // var item2 = document.getElementsByClassName('price')[i].textContent  ;    
-      // var m=document.getElementById('cartitem')
-      // var n=document.getElementById('cartprice')
-      // m.append(item2+"\n\n"); 
-      // n.append(item2+"\n\n"); 
-
+      console.log(itemprice) ;  
+      console.log(typeof(itemprice))
+  totalamount=totalamount+itemprice;
+  var amount=new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(totalamount);
+  console.log(amount) ;   
+      document.getElementById("totamt").innerHTML= "₹"+amount; 
+      
   }
+
+
+  function  buyItems(){
+    var amountalert=new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(totalamount);
+    if(totalamount>0)
+    alert("Thanks for Shopping. \nYour Total Amount to be paid is ₹"+amountalert  ); 
+    else
+    alert("You have not purchased Anything...") ; 
+  } 
 
 
  
